daa4981a72a452c747f3df371c848ff91654503e
Samuel Trujillo <sam.trujillo@drake.edu>
2023-11-27 18:24:43 -0600

    Rename Variables & Pages and addingnew Middle page

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from flask_navigation import Navigation
 # Import Azure SQL helper code
 from azuresqlconnector import *
 
+userToken = None
+
 # ============================================
 # Function to Call Sentimental AI API
 def APICall(useToken, endpoint, method, body=None):
@@ -43,22 +45,32 @@ nav.Bar('top', [
     nav.Item('API Responses', 'table')
 ])
 
+# =================================================================
 @app.route('/') 
 def index():
     return render_template('setup.html')
 
-@app.route('/form') 
+@app.route('/SubmitToken') 
 def form():
-    return render_template('newFormPage.html')
+    return render_template('SubmitTokenPage.html')
+
+@app.route('/optionsPage') 
+def optionsPage():
+    return render_template('optionsPage.html')
 
 # This function handles data entry from the form
-@app.route('/form_submit', methods=['POST']) 
+@app.route('/form_submit') 
 def form_submit():
     # -------------------------------------------
     # Get Input from User
-    form_data1 = request.form['userSentenceInput']
+    userToken = request.form['userSentenceInput']
+    return redirect(url_for('optionsPage'))
 
-    response = APICall(form_data1, 'v1/me/top/tracks?time_range=short_term&limit=10', 'GET')
+# =================================================================
+@app.route('/Top1Song', methods=['POST']) 
+def Top1Song():
+
+    response = APICall(userToken, 'v1/me/top/tracks?time_range=short_term&limit=10', 'GET')
 
     artistName = response['items'][0]["artists"][0]["name"]
     songName = response['items'][0]["name"]
@@ -95,6 +107,8 @@ def form_submit():
 
     # Redirect back to form page after the form is submitted
     return redirect(url_for('table'))
+
+# =================================================================
 
 @app.route('/table') 
 def table():
